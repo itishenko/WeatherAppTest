7361c747e758b4512edda68f09bf9ee8699a0737
itishenko <i.tishenko@gmail.com>
2026-02-20 16:14:47 +0400

    Initial Commit

---
+++ WeatherAppTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherAppTest
+//
+//  Created by Ivan Tishchenko on 20.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
